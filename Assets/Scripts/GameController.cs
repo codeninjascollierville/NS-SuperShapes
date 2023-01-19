26c4c9c29252b67ffb96531762734da73ee79e1b
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-01-18 18:29:14 -0600

    fe

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,6 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    //Hexagon game object
+    [Header("Shape Objects")]
+    public GameObject[] shapePrefabs;
+    //Spawn rate for how many
+    //objects are spawned
+    [Header("Default Spawn Delay Time")]
+    public float spawnDelay = 2f;
+    [Header("Default Spawn Time")]
+    public float  spawnTime = 3f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +23,18 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+       InvokeRepeating("Spawn", spawnDelay, spawnTime);
+    }
+    //Spawn function
+    void Spawn()
+    {
+        int randomInt = Random.Range(0, shapePrefabs.Length);
+        //Spawn new hexagon at position
+        Instantiate(shapePrefabs[randomInt], Vector3.zero, Quaternion.identity);
+    }
+    //Game over function'
+    public void GameOver()
+    {
+        CancelInvoke("Spawn");
     }
 }
